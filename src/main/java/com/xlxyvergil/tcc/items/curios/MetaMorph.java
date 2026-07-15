d231fc14ea4353477c117dde1fbbb0b608d8240b
xlxyvergil <xlxyvergil@example.com>
2026-07-15 15:39:49 +0800

    修改颜色

--- src/main/java/com/xlxyvergil/tcc/items/curios/MetaMorph.java
+++ src/main/java/com/xlxyvergil/tcc/items/curios/MetaMorph.java
@@ -173,10 +173,12 @@ tooltip.add(Component.translatable("tcc.tooltip.restricted_melee"));
         }
         tooltip.add(formatModifierTooltip(attackFromHealth * 100, "%.1f%%", Component.translatable(AttributeHelper.ATTACK_DAMAGE.getDescriptionId()))
                 .withStyle(ChatFormatting.RED));
-        tooltip.add(Component.translatable("tcc.tooltip.affected_by_max_health"));
+        tooltip.add(Component.translatable("tcc.tooltip.affected_by_max_health")
+                .withStyle(ChatFormatting.GRAY));
         tooltip.add(formatModifierTooltip(lifeStealFromResistance, "%.2f", Component.translatable(AttributeHelper.LIFE_STEAL.getDescriptionId()))
                 .withStyle(ChatFormatting.RED));
-        tooltip.add(Component.translatable("tcc.tooltip.affected_by_imaginary_resistance"));
+        tooltip.add(Component.translatable("tcc.tooltip.affected_by_imaginary_resistance")
+                .withStyle(ChatFormatting.GRAY));
         tooltip.add(Component.translatable("item.tcc.meta_morph.special_damage",
                 String.format("%.2f", imaginaryDamage))
             .withStyle(ChatFormatting.RED));
