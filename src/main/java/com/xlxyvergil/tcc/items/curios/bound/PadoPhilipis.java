3cf5764593e7a6275fdf314fa059d1419d4c1067
xlxyvergil <57478216+xlxyvergil@users.noreply.github.com>
2026-09-08 00:22:43 +0800

    钓鱼时间减少

--- src/main/java/com/xlxyvergil/tcc/items/curios/bound/PadoPhilipis.java
+++ src/main/java/com/xlxyvergil/tcc/items/curios/bound/PadoPhilipis.java
@@ -16,6 +16,7 @@ import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.TooltipFlag;
